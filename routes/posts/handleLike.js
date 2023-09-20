2d91c6e82da12f3fced6859b6c5123266d311b29
Shazzadul-Shakib <shakib1186@gmail.com>
2023-09-21 02:20:57 +0600

    create like and unlike api

--- routes/posts/handleLike.js
+++ routes/posts/handleLike.js
@@ -6,13 +6,21 @@ const router = express.Router();
 router.patch('/:id',async(req,res)=>{
     const {id}=req.params;
     const filter={_id: new ObjectId(id)};
-    const isliked = req.body.isLiked;
-    // const likedUser=req.body.likedUser;
-    console.log(isliked);
+    const newLikedUser = req.body.likeduser;
+    let likedUser=req.body.likes;
+    const targetIndex = likedUser.findIndex(
+      (item) => item.email === newLikedUser.email
+    );
+    if (targetIndex !== -1) {
+      const newArray = likedUser.filter((item) => item.email !== newLikedUser.email);
+      likedUser = newArray;
+    } else {
+      likedUser.push(newLikedUser);
+    }
+    
     const updateLikeStatus = {
       $set: {
-        isLiked: isliked,
-        // likedUser:likedUser
+        likes:likedUser
       },
     };
     const result=await postCollection.updateOne(filter,updateLikeStatus);
