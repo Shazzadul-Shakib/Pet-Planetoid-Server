f68dfe8153a857c8311c20dfcf9e775b0ee4bd80
Shazzadul-Shakib <shakib1186@gmail.com>
2023-10-25 12:12:39 +0600

    filter post by email from the same root by condition and send them to clientside

--- routes/posts/getPost.js
+++ routes/posts/getPost.js
@@ -3,10 +3,28 @@ const { postCollection } = require("../../lib/collection");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  try {
+    const userEmail = req.query.email;
+
+    if (!userEmail) {
       const result = await postCollection
         .find()
         .sort({ createdAt: -1 })
         .toArray();
-      res.send(result);
-    });
+      res.json(result);
+    }
+    else {
+      const result = await postCollection
+      .find({ email: userEmail })
+      .sort({ createdAt: -1 })
+      .toArray();
+
+    res.json(result);
+    }
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).json({ error: "An error occurred while fetching posts." });
+  }
+});
+
 module.exports = router;
