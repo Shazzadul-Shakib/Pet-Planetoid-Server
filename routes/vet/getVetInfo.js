1dbe38e624e1c8c7d14b7b363cba6c70819e936b
Shazzadul-Shakib <shakib1186@gmail.com>
2023-10-31 11:36:53 +0600

    ready to deploy

--- routes/vet/getVetInfo.js
+++ routes/vet/getVetInfo.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { vetCollection } = require("../../lib/collection");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/", async (_, res) => {
   const result = await vetCollection.find().toArray();
   res.send(result);
 });
